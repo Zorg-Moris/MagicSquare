08fcabb5a27371ae9f4c18bcbb302feafd7d9ff0
Zorg-Moris <mvr.leon@gmail.com>
2020-03-23 23:44:20 +0200

    fix update

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MagicSquare
 Forming a Magic Square
+
+Clone:
+Clone this repo to your local machine using https://github.com/Zorg-Moris/MagicSquare.git
+
+Installation:
+$ npm install
+
+Tests:
+$ npm run test
+
+Start:
+open the file - index.html  in your browser.
+open the file - index.html in your browser. All information will be displayed in the customizer:
+1. The starting array.
+2. The modified array.
+3. Total Cost.
+4 The time spent on changing the array according to the task.
+
+to test with another array, there is another test array in the index.js file, you can also create your own array according to the conditions of the task
+
---
+++ src/index.js
@@ -0,0 +1,115 @@
+"use strict";
+
+function displayStartSquare(array, id) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            let cell = document.getElementById(id);
+            let div = document.createElement("div");
+            div.classList.add("cell");
+            div.innerHTML = array[i][j];
+            cell.appendChild(div);
+        }
+    }
+};
+
+
+function displayInfo(total, timer) {
+    let res = document.getElementById("result");
+    res.innerHTML = total;
+    let time = document.getElementById("timer");
+    time.innerHTML = timer;
+}
+
+//test array
+// let magicSquare = [
+//     [4, 7, 1],
+//     [5, 9, 2],
+//     [8, 6, 5]
+// ];
+
+let magicSquare = [
+    [4, 8, 2],
+    [4, 5, 7],
+    [6, 1, 6]
+];
+
+let newMagicSquare = magicSquare;
+const magicConst = 15;
+
+
+function magicSquareFunc(magicSquare) {
+
+    let totalCoast = 0;
+    let digitCount = 0;
+
+    for (let i = 0, length = magicSquare.length; i < length; i++) {
+        let resultColumn = 0;
+        let resultRow = 0;
+        let arrayColumn = [];
+        let arrayRow = [];
+
+        for (let j = 0, length = magicSquare.length; j < length; j++) {
+            if (j >= i) {
+                arrayColumn.push(magicSquare[j][i]);
+            }
+            resultColumn = magicSquare[j][i] + resultColumn;
+        }
+
+        if (resultColumn !== magicConst) {
+            let difference = Math.abs(resultColumn - magicConst);
+            if (resultColumn > magicConst) {
+                let index = arrayColumn.indexOf(Math.max(...arrayColumn));
+                let res = arrayColumn[index] - difference;
+                totalCoast = totalCoast + difference;
+                newMagicSquare[index + digitCount][i] = res;
+            }
+
+            if (resultColumn < magicConst) {
+                let index = arrayColumn.indexOf(Math.min(...arrayColumn));
+                let res = arrayColumn[index] + difference;
+                totalCoast = totalCoast + difference;
+                newMagicSquare[index + digitCount][i] = res;
+            }
+        }
+
+        digitCount = i === 1 ? digitCount + 1 : digitCount;
+
+        for (let j = 0, length = magicSquare.length; j < length; j++) {
+            if (j >= i) {
+                arrayRow.push(magicSquare[i][j]);
+            }
+            resultRow = magicSquare[i][j] + resultRow;
+        }
+
+        if (resultRow !== magicConst) {
+            let difference = Math.abs(resultRow - magicConst);
+            if (resultRow > magicConst) {
+                let index = arrayRow.indexOf(Math.max(...arrayRow));
+                let res = Math.abs(arrayRow[index] - difference);
+                totalCoast = totalCoast + difference;
+                newMagicSquare[i][index + i] = res;
+            }
+
+            if (resultRow < magicConst) {
+                let index = arrayRow.indexOf(Math.min(...arrayRow));
+                let res = Math.abs(arrayRow[index] + difference);
+                totalCoast = totalCoast + difference;
+                newMagicSquare[i][index + digitCount] = res;
+            }
+        }
+        digitCount = digitCount !== 2 ? digitCount + 1 : digitCount;
+    }
+
+    return totalCoast;
+}
+
+
+let startTime = performance.now();
+let totalCoast = magicSquareFunc(newMagicSquare);
+let endTime = performance.now();
+
+let timer = endTime - startTime;
+
+displayStartSquare(magicSquare, "firstSquare");
+displayStartSquare(newMagicSquare, "resSquare");
+displayInfo(totalCoast, timer);
